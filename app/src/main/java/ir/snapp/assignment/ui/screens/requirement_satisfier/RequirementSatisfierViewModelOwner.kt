eae9d940f0bdd15993883680d82f5af7e2133eb8
Sepehr Sadri <sepehrsadri@gmail.com>
2020-06-25 21:39:34 +0430

    Add requirement satisfier fragment and view model
    + this screen provide location & gps permission

---
+++ app/src/main/java/ir/snapp/assignment/ui/screens/requirement_satisfier/RequirementSatisfierViewModelOwner.kt
@@ -0,0 +1,11 @@
+package ir.snapp.assignment.ui.screens.requirement_satisfier
+
+import ir.snapp.assignment.ui.utils.owner_view_model.BaseViewModelOwner
+
+/**
+ * Created by Sepehr Sadri on 6/25/2020.
+ * sepehrsadri@gmail.com
+ * Tehran, Iran.
+ * Copyright © 2020 by Sepehr Sadri. All rights reserved.
+ */
+interface RequirementSatisfierViewModelOwner : BaseViewModelOwner
