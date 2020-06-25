3163bd466b16c32ccd36bbd6a067d4e974573668
Sepehr Sadri <sepehrsadri@gmail.com>
2020-06-25 21:24:39 +0430

    Add dashboard activity

---
+++ app/src/main/java/ir/snapp/assignment/ui/screens/dashboard/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package ir.snapp.assignment.ui.screens.dashboard
+
+import android.os.Bundle
+import ir.snapp.assignment.R
+import ir.snapp.assignment.ui.utils.BaseActivity
+
+class DashboardActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
--- app/src/main/java/ir/snapp/assignment/ui/utils/BaseActivity.kt
+++ app/src/main/java/ir/snapp/assignment/ui/utils/BaseActivity.kt
@@ -2,6 +2,8 @@ package ir.snapp.assignment.ui.utils
 
 import android.annotation.SuppressLint
 import dagger.android.support.DaggerAppCompatActivity
+import ir.snapp.assignment.components.permission.PermissionProvider
+import javax.inject.Inject
 
 /**
  * Created by Sepehr Sadri on 6/25/2020.
@@ -10,4 +12,16 @@ import dagger.android.support.DaggerAppCompatActivity
  * Copyright © 2020 by Sepehr Sadri. All rights reserved.
  */
 @SuppressLint("Registered")
-abstract class BaseActivity : DaggerAppCompatActivity()
+abstract class BaseActivity : DaggerAppCompatActivity() {
+    @Inject
+    lateinit var permissionProvider: PermissionProvider
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (permissionProvider.onPermissionResult(requestCode).not())
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+}
