a13936f4a2730a6bc0e63e54fda1204f3ea10534
Sepehr Sadri <sepehrsadri@gmail.com>
2020-06-25 16:14:01 +0430

    Add base application

---
+++ app/src/main/java/ir/snapp/assignment/App.kt
@@ -0,0 +1,16 @@
+package ir.snapp.assignment
+
+import android.app.Application
+
+/**
+ * Created by Sepehr Sadri on 6/25/2020.
+ * sepehrsadri@gmail.com
+ * Tehran, Iran.
+ * Copyright © 2020 by Sepehr Sadri. All rights reserved.
+ */
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
